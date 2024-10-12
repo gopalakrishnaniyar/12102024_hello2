633143ea2e59fd91c51f64eb4df57234d2e9c9e4
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-10-12 17:07:46 +0300

    vars and decimals

--- hello7/src/main/scala/main.scala
+++ hello7/src/main/scala/main.scala
@@ -10,4 +10,13 @@ Unit = {
   val quote ="The essence of Scala: Fusion of functional and object-oriented programming in a typed setting."
     println(quote)
   println(s"$mi$lastName")
+
+  val myList = List(1,2,3,4,5)
+  println(myList.size < 6)
+
+  val temper = 399834.00923
+  println(temper)
+
+  val voyagerDistance = BigDecimal(23234534545.3434)
+  println("voyager distance is " + voyagerDistance)
 }
