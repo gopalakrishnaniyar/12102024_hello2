b5154e2f810aeaf186e2d8ed19042dc1aaecc093
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-10-12 14:31:06 +0300

    created simple main function inside a definition called main and then declares a Unit {} to print mutuable var, and immutable vals

--- hello7/src/main/scala/main.scala
+++ hello7/src/main/scala/main.scala
@@ -1,7 +1,13 @@
-@main
-def main(): Unit = {
-  println("Hello world!")
-  val nums = List(1, 2, 3)
-  println(nums.map(_ * 2) )
-}
+import scala.io.StdIn.readLine
 
+@main
+def popop():
+Unit = {
+  val firstName = "John"
+  val mi = 'C'
+  val lastName = "Doe"
+  println(s"Full Name is : $firstName $mi $lastName")
+  val quote ="The essence of Scala: Fusion of functional and object-oriented programming in a typed setting."
+    println(quote)
+  println(s"$mi$lastName")
+}
