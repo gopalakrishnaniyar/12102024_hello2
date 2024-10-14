93c552cbb333da427e15da63dd90ce3eb4772adf
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-10-14 21:26:40 +0300

    Domain Modelling , Classes and Traits

--- Traits and Classes/domainModelling/src/main/scala/main.scala
+++ Traits and Classes/domainModelling/src/main/scala/main.scala
@@ -1,8 +1,37 @@
 package org.domainModelling.com
 
+trait authenticate:
+  def methodOne(): String // has no body, so it’s abstract
+  def methodTwo():
+  Unit = {
+    println("I am in method 2")
+  }
+
+class myClass() extends authenticate:
+  def methodOne(): String = "Woof!"
+  override def methodTwo(): Unit = {
+    println("overriding method 2")
+  }
+
+class myClass2(a: String, b: String):
+
+  var a1 = "string32"
+
+  def myMethod():
+  Unit = { println(s"values are: $a and $b and $a1") }
+
 
 @main
 def main(): Unit = {
-  println("Hello world!")
+  //println("Hello world!")
+  var a = new myClass()
+  print("\n" + a.methodOne() +  "\n")
+  a.methodTwo()
+
+  // instantiate class myclass2 an access method and direct class variable.
+  var obj = myClass2("zun "," zoo")
+  obj.myMethod()
+  println (obj.a1)
+
 }
 
