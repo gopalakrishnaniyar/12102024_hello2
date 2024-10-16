9fd036aa031bafe3a1f805096dc1b6508ef4205d
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-10-16 13:28:30 +0300

    case class deep dive. Immutable encouragement

--- Case Class Features/caseClasses/src/main/scala/main.scala
+++ Case Class Features/caseClasses/src/main/scala/main.scala
@@ -12,10 +12,13 @@ case class caseClass(x:String)
 def main(): Unit = {
 
   val obj1 = new normalClass("normal class")
-  val obj2 = caseClass("case class.")
+  val obj2 = caseClass("inside case class.")
 
   println(obj1.x + obj1.temp1)
   println(obj2.x)
+
+  val valObj = obj2.copy(x = "lol")
+  println(valObj.toString)
 
 }
 
