632dd459964092620df13edc5ab4e97edbd45b38
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-10-20 17:54:28 +0300

    Higher Order Functions

--- Higher Order Functions/higherOrderFunctions/src/main/scala/main.scala
+++ Higher Order Functions/higherOrderFunctions/src/main/scala/main.scala
@@ -9,11 +9,25 @@ def main(): Unit = {
   println( myList.map(i => i*2) )
   println( myList.map(_ *2) )
 
+  //
+  val temp = myList.map(i => doubleWrapper(i))
+  println(temp)
+
   // <- operator is the generator typically used in for loop for iterating collections
 
   for(item <- myList) do println(item*2)
   val newList = for(item <- myList) yield (item*2)
   println(newList)
+
+  //
+  println(myList.map(i => i.toDouble))
+
+  val numbes = (1 to 10).toList
+  println(numbes)
+
+  // immutable list multiple
+  val newNumbers = numbes.filter(i => i % 2 == 0).filter(_ > 4).map(_ * 10)
+  println(newNumbers)
 
 }
 
