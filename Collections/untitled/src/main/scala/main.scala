b45850d1c3d088c99966673261d5e6f80f33a339
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-10-23 08:20:53 +0300

    list exploration

--- Collections/untitled/src/main/scala/main.scala
+++ Collections/untitled/src/main/scala/main.scala
@@ -7,8 +7,12 @@ def main(): Unit = {
   val b = (1 to 10).toList
   val c = (1 until 10).toList
   val d = (1 to 10 by 3).toList
+  val f = List.range(1, 5)
+  val g = List.range(1, 10, 3)
   println(b)
   println(c)
   println(d)
+  println(f)
+  println(g)
 }
 
