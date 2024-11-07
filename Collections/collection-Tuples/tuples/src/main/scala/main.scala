965b9b785f8e3e27d7c48b8650b0a073123672cc
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-11-07 09:05:10 +0200

    Tuples introduction

--- Collections/collection-Tuples/tuples/src/main/scala/main.scala
+++ Collections/collection-Tuples/tuples/src/main/scala/main.scala
@@ -9,7 +9,7 @@ def main(): Unit = {
   println("Reduce Left: " + myLister32.reduceLeft((a, b) => a + b))
 
   val myTuple = (8,myLister32, "12", 6.5)
-  println(myTuple)
+  println(myTuple(0))
 
 
 }
