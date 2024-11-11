8cdd160a1212e27dd2c9828c2ef572b3758e8c24
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-11-11 08:51:24 +0200

    case class interesting use case

--- caseClass/src/main/scala/main.scala
+++ caseClass/src/main/scala/main.scala
@@ -1,6 +1,15 @@
+case class Address(city: String,street : String)
+case class Person(name : String, age: Int)
 
 @main
 def main(): Unit = {
-  println("Hello world!")
+  val addresses = List(Address("New York","5th Avenue"),Address("Manhattan","6th Avenue"))
+  val sortedAddress = addresses.sortBy(item => (item.city,item.street))
+  println(addresses)
+  println(sortedAddress)
+  sortedAddress.foreach(println)
+
+  val person1 = Person("Alice",30)
+  println(person1.name)
 }
 
