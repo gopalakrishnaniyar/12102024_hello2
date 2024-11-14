750184d4057a9e7ca6b9b3595f69b45159008d91
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-11-14 12:18:41 +0200

    Method of type any accepts any data type

--- Scala Types/untitled/src/main/scala/main.scala
+++ Scala Types/untitled/src/main/scala/main.scala
@@ -4,10 +4,17 @@ def main(): Unit = {
   println("Hello world!")
   myMethod("j")
   myMethod(66.34)
+  myMethod("66.34")
+  myMethod2(557)
 }
 
 def myMethod(a:Any):
 Unit = {
-  println("hello from mymethod." + a)
+  println("hello from mymethod." + a )
+}
+
+def myMethod2(a:Int):
+Unit = {
+  println("hello from mymethod2." + a )
 }
 
