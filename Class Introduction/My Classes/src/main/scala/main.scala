610c597a859b3c4843fa47e348cca9da1382cf81
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-11-18 06:19:57 +0200

    classes and methods

--- Class Introduction/My Classes/src/main/scala/main.scala
+++ Class Introduction/My Classes/src/main/scala/main.scala
@@ -1,11 +1,13 @@
-class myClass1(var i: String)
+class myClass1(var i: String,var j:Int)
 {
-  def myMethod(): Unit={ println("testeting..." + i) }
+  def myMethod(): Unit={ println("testing..." + i) }
+  def myMethod2(): Int={ return j+10 }
 }
 
 @main
 def main(): Unit = {
-  val obj = new myClass1("Ä")
-  obj.myMethod() // This will only print "printing..Ä"
+  val obj = new myClass1("Ä",23)
+  obj.myMethod()
+  println(obj.myMethod2())
 }
 
