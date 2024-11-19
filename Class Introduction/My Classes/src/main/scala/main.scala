c3ea0eec5468908b9dcd7600801990760190f851
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-11-19 08:26:12 +0200

    classes and methods

--- Class Introduction/My Classes/src/main/scala/main.scala
+++ Class Introduction/My Classes/src/main/scala/main.scala
@@ -1,9 +1,12 @@
 class myClass1(var i: String,var j:Int)
 {
   def myMethod(): Unit={ println("testing..." + i) }
-  def myMethod2(): Int={
-    val num1 = scala.io.StdIn.readLine().toInt
-    j+num1
+  def myMethod2(): Int=
+  {
+
+    val num1 = scala.io.StdIn.readLine()
+    val num2 = scala.io.StdIn.readLine()
+    return num1.toInt + num2.toInt
   }
 }
 // no need of return keyword. just grab the arg from class param and use it inside method
