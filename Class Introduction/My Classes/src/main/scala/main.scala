2fb4c04b07d9d0caa8e6284e0faa344c0d16a52d
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-11-20 02:42:32 +0200

    classes and methods

--- Class Introduction/My Classes/src/main/scala/main.scala
+++ Class Introduction/My Classes/src/main/scala/main.scala
@@ -6,6 +6,6 @@ object solution{
 // no need of return keyword. just grab the arg from class param and use it inside method
 @main
 def main(): Unit = {
-  println(solution)
+  println(solution.toString)
 }
 
