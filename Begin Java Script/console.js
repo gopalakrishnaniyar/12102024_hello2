8d46f5c19a7fb55d46c323c260b3c50885b13323
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-09 10:34:55 +0200

    ok

--- Begin Java Script/console.js
+++ Begin Java Script/console.js
@@ -1,1 +1,3 @@
-console.log('my first java script')
+let name1 = "this is string 1";
+let name2 = "this is string 2";
+console.log(`hello ${name1}`) // Well this is workday extend pmd scripting for heaven sake
