0256ba020d6123c7937b22674cb8df250627417c
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-11 09:43:46 +0200

    asynch vs sync processing

---
+++ my-node-project/node-file-system.js
@@ -0,0 +1,20 @@
+console.log('learning about file sytem')
+const fs = require('fs')
+
+
+//Writing contents into a file
+
+/*fs.writeFile('C:/Users/sivakgop/OneDrive - Tietoevry/Desktop/message.txt', 'hello', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});*/
+
+const jpayload = fs.readFile('C:/Users/sivakgop/OneDrive - Tietoevry/Desktop/message.txt', 'utf8', (err,data) => {
+    if (err) throw err;
+  });
+
+  console.log(jpayload)
+
+  // this will not work as it processes asynchronously.  jpayload cannot be referenced before it fully executes
+  // to process it
+
