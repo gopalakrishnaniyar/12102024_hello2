5282371d725e0ec228e0b69f73743224c1d687c0
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-17 09:37:42 +0200

    filter in array

--- node-with-Atlas/problemSolving.js
+++ node-with-Atlas/problemSolving.js
@@ -1,4 +1,5 @@
 //https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
+//filter function in array 
 const arr = [1,1,0,-1,-1]
 console.log(arr.filter(item => item < 0).length / arr.length)
 console.log(arr.filter(item => item > 0).length / arr.length)
