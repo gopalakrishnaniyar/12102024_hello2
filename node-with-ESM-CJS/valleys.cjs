4a4230f98e54a62186238bbe8379a169ac9b0bbd
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-23 05:12:25 +0200

    counting valleys

--- node-with-ESM-CJS/valleys.cjs
+++ node-with-ESM-CJS/valleys.cjs
@@ -1,3 +1,17 @@
-const steps = 'UDDDUDDUU'
-const stepArray = steps.split('');
-console.log(Math.abs(stepArray.filter(item => item == 'U').length - stepArray.filter(item => item == 'D').length))
+function countingValleys(steps, path) {
+const stepArray = path.split('');
+var baseLevel = 0 ;
+var valleyCounter = 0;
+for (let index = 0; index < stepArray.length; index++) {
+    const element = stepArray[index];
+    
+    if (element === 'U') {
+        baseLevel++;
+    } else {
+        baseLevel--;
+    }
+    if(baseLevel == 0 && stepArray[index] === 'U') {valleyCounter++;}
+}
+return valleyCounter;
+
+}
