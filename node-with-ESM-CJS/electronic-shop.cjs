97177810f81a79d2ae59d722893c8c5b8b8c282a
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-24 05:17:20 +0200

    ok ok done

--- node-with-ESM-CJS/electronic-shop.cjs
+++ node-with-ESM-CJS/electronic-shop.cjs
@@ -1,3 +1,22 @@
-const budget = 60;
-const keyBoard = [40,50,60];
-const udbDrives = [5,8,12];
+function getmaxItems(keyBoard,usbDrives,b,temp){
+
+//const b = 60;
+//const keyBoard = [50,40,60];
+//const usbDrives = [5,8,12,9];
+
+
+console.log(temp.split(' '))
+
+const keyBoardSort  = keyBoard.sort((x, y) => y-x)
+//console.log(keyBoardSort)
+const usbDrivesSort = usbDrives.sort((x, y) => y-x)
+//console.log(usbDrivesSort)
+for(const item of keyBoardSort){
+    for(const drive of usbDrivesSort){
+        if( item+drive <= b){return item+drive}
+    }
+}
+}
+
+const totals = getmaxItems([50,40,60],[5,8,12],60,'50 20 21');
+console.log(totals);
