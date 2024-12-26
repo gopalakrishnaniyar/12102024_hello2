137aee386c5738058caccb32e1e75fa9a0059c14
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-26 10:19:07 +0200

    array and inf entries inside array

--- node-with-ESM-CJS/node-list.cjs
+++ node-with-ESM-CJS/node-list.cjs
@@ -1,4 +1,4 @@
 /* Includes and Find difference */
 const myList = [1,2,3,4,2];
 console.log(myList.find(item => item == 83));
-console.log(myList.includes(2));
+console.log(myList.includes(2)); //returns true r false
