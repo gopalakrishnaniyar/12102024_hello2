742e61f5e2839d54abdb32c126c11fbd0c408fae
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2024-12-28 13:25:16 +0200

    ok

--- node-with-ESM-CJS/node-list.cjs
+++ node-with-ESM-CJS/node-list.cjs
@@ -1,4 +1,6 @@
 /* Includes and Find difference */
-const myList = [1,2,3,4,2];
-console.log(myList.find(item => item == 83));
-console.log(myList.includes(2));
+const myList = [-2,1,2,3,4,2,-5];
+// console.log(myList.find(item => item == 83));
+// console.log(myList.includes(2));
+console.log(myList.sort((a,b) => a-b))
+console.log(myList.sort((a,b) => b-a))
