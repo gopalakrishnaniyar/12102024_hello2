907b04d383961db19342b2eef4d25f97b69273e6
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2025-01-01 10:06:38 +0200

    circularRotation

--- node-with-ESM-CJS/rotating-circle.cjs
+++ node-with-ESM-CJS/rotating-circle.cjs
@@ -1,11 +1,8 @@
-function circularArrayRotation(a, k, queries) {
+function circularArrayRotation(a, k, queries) 
+{
 
-    const range = Array.from({ length: k }, (_, item) => item + 1)
-    range.forEach(element => {
-        a.unshift(a[a.length - 1])
-        a.pop()
-    });
-    return queries.filter(item => a[item] != null).map(item => a[item])
+        Array.from({ length: k }, (_, item) => item + 1).forEach(element => {a.unshift(a[a.length - 1]);  a.pop(); });
+        return queries.filter(item => a[item] != null).map(item => a[item]);
 }
 
 const results = circularArrayRotation([3, 4, 5], 2, [1, 2])
