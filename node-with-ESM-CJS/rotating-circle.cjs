da21fd525c59304509ed2560fd0f13dd6e1fcec2
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2025-01-01 08:18:59 +0200

    rotate circle pop

--- node-with-ESM-CJS/rotating-circle.cjs
+++ node-with-ESM-CJS/rotating-circle.cjs
@@ -1,3 +1,10 @@
 var myArray = [5,3,2];
-myArray.shift();
-console.log(myArray);
+const round = 2
+const range = Array.from({length:round}, (_ ,item) => item + 1)
+
+range.forEach(element => {
+    myArray.unshift(myArray[myArray.length - 1])
+    myArray.pop()
+});
+
+console.log(myArray)
