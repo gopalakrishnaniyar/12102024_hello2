e8a3ac2a27fe2cb2f6f8774c9979aa4622ab44e6
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2025-01-02 06:05:56 +0200

    more of arrays

---
+++ node-with-ESM-CJS/more-arrays.cjs
@@ -0,0 +1,2 @@
+const myArray = [1,2,3,42,98,1,4,5]
+console.log(myArray.length)
--- node-with-ESM-CJS/rotating-circle.cjs
+++ node-with-ESM-CJS/rotating-circle.cjs
@@ -1,9 +1,11 @@
 function circularArrayRotation(a, k, queries) 
 {
 
-        Array.from({ length: k }, (_, item) => item + 1).forEach(element => {a.unshift(a[a.length - 1]);  a.pop(); });
-        return queries.filter(item => a[item] != null).map(item => a[item]);
+    Array.from({ length: k }, (_, item) => item + 1).forEach(element => {a.unshift(a[a.length - 1]);  a.pop(); });
+    return queries.filter(item => a[item] != null).map(item => a[item]);
 }
 
 const results = circularArrayRotation([3, 4, 5], 2, [1, 2])
 console.log(results)
+
+
