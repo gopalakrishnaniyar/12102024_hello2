40b045c71fe553324b7617ee3778a4982e196060
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2025-01-04 08:03:09 +0200

    constant reduceRight

--- node-with-ESM-CJS/tester03012025.cjs
+++ node-with-ESM-CJS/tester03012025.cjs
@@ -1,3 +1,3 @@
 const myArray = [1,8,9];
-const sumRight = myArray.reduceRight((acc, cur) => acc + cur, 42 )
+const sumRight = myArray.reduceRight((acc, cur) => acc + cur, 42 ) // 42 is constant final value
 console.log(sumRight)
