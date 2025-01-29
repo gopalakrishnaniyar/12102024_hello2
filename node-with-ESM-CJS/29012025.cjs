5abea775e6e1901b0a9f10855065d4f29e6c5f56
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2025-01-29 16:39:35 +0200

    auto setting

--- node-with-ESM-CJS/29012025.cjs
+++ node-with-ESM-CJS/29012025.cjs
@@ -6,10 +6,17 @@ console.log(maxNum)
 // All about date functions
 const currentDate = new Date();
 console.log(currentDate);
-console.log(currentDate.getHours());
-console.log(currentDate.getMonth() + 1);
-console.log(currentDate.toISOString().split('T')[0]);
+const yr   = currentDate.getFullYear();
+const day = currentDate.getDate();
+const mnth = currentDate.getMonth() + 1;
+console.log(yr + '' + mnth + '' + day);
 
+// add 1 month to curretnDate
+currentDate.setMonth(currentDate.getMonth()+1);
 console.log(currentDate);
 
+//since feb doesnt have feb 29. a date incrementing to +1 on jan 29, takes the next month to Feb 01.. lovely 
+//Wed Jan 29 2025 16:37:17 GMT+0200 (Eastern European Standard Time)
+//2025129
+//Sat Mar 01 2025 16:37:17 GMT+0200 (Eastern European Standard Time)
 
