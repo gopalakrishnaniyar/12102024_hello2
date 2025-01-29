07e61c1ffc935eaa6a951b115462ade40f30a72b
gopalakrishnaniyar <gopalakrishnaniyar@outlook.com>
2025-01-29 13:31:12 +0200

    ok

--- node-with-ESM-CJS/29012025.cjs
+++ node-with-ESM-CJS/29012025.cjs
@@ -2,3 +2,14 @@
 var myList = [12121,1,34,-1,2,34,787,999];
 var maxNum = myList.reduce((max,num) => (num >max ? num : max), myList[0]);
 console.log(maxNum)
+
+// All about date functions
+const currentDate = new Date();
+console.log(currentDate);
+console.log(currentDate.getHours());
+console.log(currentDate.getMonth() + 1);
+console.log(currentDate.toISOString().split('T')[0]);
+
+console.log(currentDate);
+
+
